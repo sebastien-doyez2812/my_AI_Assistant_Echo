d829e1fff373cb0f1fd752daf7b17cb73f78c070
sebastien doyez <doyez.sebastien34090@gmail.com>
2024-11-10 21:01:18 -0500

    feature1/ first functions

--- include/functions.py
+++ include/functions.py
@@ -4,6 +4,7 @@ import re
 import mysql
 import pyttsx3
 import speech_recognition as sr
+import webbrowser
 from datetime import datetime
 import json
 import requests
@@ -26,6 +27,61 @@ translator = Translator()
 # Fast research:
 user_agent_wikipedia = "JarvisPython/1.0 (https://github.com/sebastien-doyez2812/AI-projects/AI_ChatBot)"
 wiki = wikipediaapi.Wikipedia(user_agent_wikipedia, 'fr')
+
+
+def process_command(command, data, answer):
+    # Basic interaction:
+    print(command)
+    print(data["meteo"])
+    if any(keyword in command for keyword in data["salutation"]):
+        speak(f"Bonjour {USER}, Comment puis je vous aider aujourd'hui")
+        return 
+    
+    if any(keyword in command for keyword in data["presentation"]):
+        speak(answer["presentation"])
+        return
+
+    if  any(keyword in command for keyword in data["veille"]):
+        speak(answer["veille"])
+        return
+
+    # Basic fonctionnalities:
+    # What time is it?
+    if any(keyword in command for keyword in data["heure"]):
+        get_hour()
+        return
+
+    # What's the weather?
+    if any(keyword in command for keyword in data["meteo"]):
+        query = re.search(r"météo pour (.+)", command) or re.search(r"météo à (.+)", command)
+        if query == None:
+            get_weather()
+        else:
+            get_weather_at(query.group(1))
+        return
+
+    # Where are we?
+    if any(keyword in command for keyword in data["localisation"]):
+        city, region = get_localisation()
+        speak(f"Nous sommes a {city}, {region}")
+        return
+    
+    # Search on the web
+    if any(keyword in command for keyword in data["youtube"]):
+        query = re.search(r"recherche sur Youtube (.+)", command)
+        if query != None:
+            youtube(query.group(1))
+        return
+
+    if any(keyword in command for keyword in data["recherche rapide"]):
+        print("recherche rapide")
+        do_fast_research(command)
+        return
+    
+    if any(keyword in command for keyword in data["recherche"]):
+        query = re.search(r"cherche (.+)", command) 
+        search(query.group(1))
+        return
 
 def speak(sentence):
     """
@@ -102,7 +158,6 @@ def get_localisation():
     """
     try:
         response = requests.get("https://ipinfo.io")
-        print(response)
         data = response.json()
         city = data["city"]
         region = data["region"]
@@ -120,13 +175,10 @@ def get_weather():
     """
     try:
         api_key_meteo = read_text_from_json(PATH_PARAMETERS)["api_meteo"][0]
-        print(api_key_meteo)
         city, _ = get_localisation()
-        print(city)
         url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key_meteo}&units=metric"
         response = requests.get(url)
         data = response.json()
-        print(data)
         if data["cod"] != "404":
 
             # Get the data:
@@ -148,7 +200,7 @@ def do_fast_research(command):
     ARGS: 
         the command( what JARVIS heard)
     """
-    query = re.search(r"recherche rapide sur (.+)", command) or re.search(r"Qu'est ce qu'un (.+)", command) or re.search(r"Qu'est ce qu'une (.+)", command) or re.search(r"Reherche rapidement (.+)", command)
+    query = re.search(r"recherche rapide sur (.+)", command) or re.search(r"Qu'est ce qu'un (.+)", command) or re.search(r"Qu'est ce qu'une (.+)", command) or re.search(r"Recherche rapidement (.+)", command)
     print(query)
     if query != None:
         page = wiki.page(query.group(1))
@@ -161,8 +213,53 @@ def do_fast_research(command):
             command = listen()
 
             if "oui" in command:
-                print("oui")
-                #TODO
+                search(query.group(1))
+                speak(f"Voici les résultats pour {query.group(1)}")
     else:
         #TODO: a mettre dans le json
         speak("Je n'ai pas compris votre demande")
+
+def search(query):
+    """
+    Do a research and open the web browser for a query
+
+    ARGS:
+        query: string, what we want to research
+    """
+    url = f"https://www.google.com/search?q={query}"
+    webbrowser.open(url)
+    speak(f"Voici les résultats pour {query} sur Google.")
+
+def get_weather_at(city):
+    """
+    Give us the weather at a specific place
+    
+    ARGS:
+        city: string
+    """
+    url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={read_text_from_json(PATH_PARAMETERS)["api_meteo"][0]}&units=metric"
+    try: 
+        response = requests.get(url)
+        data = response.json()       
+        if data["cod"] != "404": #error code
+            weather = data["main"]
+            temp = weather["temp"]
+            desc = data["weather"][0]["description"]
+            description = translator.translate(desc, src='en', dest='fr')
+            speak(f"La température à {city} est de {temp} degrés Celsius, avec {description.text}")
+        else:
+            speak(f"Je n'ai pas pu trouver la météo pour {city}")
+    except Exception as e:
+        speak(f"Je n'ai pas pu récupérer les données météos. Erreur {str(e)}")
+
+
+def youtube(request):
+    """
+    Research on Youtube
+
+    ARGS:
+        request: string, what we want to research on Youtube
+    """
+    url = f"https://www.youtube.com/results?search_query={request}"
+    webbrowser.open(url)
+    speak(f"Voici les résultats pour {request} sur Youtube.")
--- main.py
+++ main.py
@@ -2,32 +2,6 @@ import threading
 from include.functions import *
 from include.header import *
 
-def process_command(command, data, answer):
-    # Basic interaction:
-    print(command)
-    print(data["meteo"])
-    if any(keyword in command for keyword in data["salutation"]):
-        speak(f"Bonjour {USER}, Comment puis je vous aider aujourd'hui")
-    if any(keyword in command for keyword in data["presentation"]):
-        speak(answer["presentation"])
-    if  any(keyword in command for keyword in data["veille"]):
-        speak(answer["veille"])
-
-    # Basic fonctionnalities:
-    if any(keyword in command for keyword in data["heure"]):
-        get_hour()
-    if any(keyword in command for keyword in data["meteo"]):
-        get_weather()
-    if any(keyword in command for keyword in data["localisation"]):
-        city, region = get_localisation()
-        speak(f"Nous sommes a {city}, {region}")
-    if any(keyword in command for keyword in data["recherche rapide"]):
-        print("recherche rapide")
-        do_fast_research(command)
-
-
-
-
 def main():
     data = read_text_from_json(PATH_TEXT_JSON)
     answer = read_text_from_json(PATH_ANSWER_JSON)
